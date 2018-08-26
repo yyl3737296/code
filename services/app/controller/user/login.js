fedeef449b25915cf13ca8b403465d51280d656c
Aaron <297303831@qq.com>
2018-08-26 13:03:33 +0800

    pem

--- services/app/controller/user/login.js
+++ services/app/controller/user/login.js
@@ -3,17 +3,17 @@
 const Controller = require('egg').Controller;
 
 class LoginController extends Controller {
-  async generateToken(data, time) {
+  generateToken(data, time) {
     const jwt = require('jsonwebtoken');
     const path = require('path');
     const fs = require('fs');
 
     let created = Math.floor(Date.now() / 1000);
-    //let cert = fs.readFileSync(path.join(__dirname, '../../public/rsa_private_key.pem'));//私钥
+    let cert = fs.readFileSync(path.join(__dirname, '../../public/rsa_private_key.pem'));//私钥
     let token = jwt.sign({
         data,
         exp: created + time
-    }, 'aaronyang', {algorithm: 'RS256'});
+    }, cert, {algorithm: 'RS256'});
     return token;
   }
   async login() {
@@ -26,7 +26,7 @@ class LoginController extends Controller {
     let token = this.generateToken({_id: '123123123123'}, 60000);
 
     console.log(token+'===================='+password);
-    ctx.body = {success: 1, name: '123123123'};
+    ctx.body = {success: 0, token: token};
   }
 }
 
