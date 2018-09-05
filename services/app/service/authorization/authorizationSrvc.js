59a885744da8d699c87875338aa2325c43045724
Aaron <297303831@qq.com>
2018-09-05 22:26:25 +0800

    mongodb数字转整型

--- services/app/service/authorization/authorizationSrvc.js
+++ services/app/service/authorization/authorizationSrvc.js
@@ -4,7 +4,7 @@ class authorizationSrvc extends Service {
   async get(page, size, query) {
     const { app } = this;
     let start = ( page - 1 ) * size;
-    const result = await app.mongo.find('User', {"query": query, "limit":size, "skip": start, "sort": { index: -1 }, "project": {'password':0}});
+    const result = await app.mongo.find('User', {"query": query, "limit": parseInt(size), "skip": start, "sort": { index: -1 }, "project": {'password':0}});
     return result;
   }
 
